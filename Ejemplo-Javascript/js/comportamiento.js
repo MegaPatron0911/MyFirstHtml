9119189e278ba8e3850025c682f00d250707ef4a
MegaPatron0911 <oswaldopatron0911@gmail.com>
2025-03-26 18:52:56 -0500

    Add files via upload

---
+++ Ejemplo-Javascript/js/comportamiento.js
@@ -0,0 +1,5 @@
+document.write("<section class='galeria'>");
+for(let i=0; i<=10; i++){
+    document.write("<img class='imagenGaleria' src='img/MartinFoto"+i+".jpeg'>");
+}
+document.write("</section>");
